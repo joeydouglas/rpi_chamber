bbdc7fffb8503679c05f35ea4858b16daefaa3e6
joeyd <joey.douglas@gmail.com>
2016-12-28 23:16:19 +0000

    began work on relayAdjustment function. moved influxdb output to its own function

--- chamber_script4.py
+++ chamber_script4.py
@@ -16,14 +16,12 @@ dbPort = "8086"
 dbName = "test"
 #Name of the curing chamber. Mostly useful if you have more than one.
 chamberName = "meatsack"
-#Celcius or Fahrenheit (must be C or F).
-temperatureUnits = "F"
 #Desired temp/humidity set here.
-desiredTemperature = 55
+desiredTemperature = 20.5
 desiredHumidity = 80
 #Set the drift or fluctuation allowed before outputs are changed to adjust the temp/humidity.
-driftTemperature = 5
-driftHumidity = 5
+driftTemperature = 2
+driftHumidity = 2
 
 
 #Set timestamp in seconds. Only change if you need time accuracy beyond seconds.
@@ -31,6 +29,7 @@ seconds = int(time.time())
 
 #Set GPIO pin specification. DO NOT CHANGE!
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 #----------------------------
 #Setup temp/humidity sensors.
@@ -92,38 +91,46 @@ headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 payload = "q=CREATE DATABASE %s\n" % dbName
 r = requests.post(url, data=payload, headers=headers)
 
-
 #Function for getting temp/humidity readings.
 def sensorReading():
   print "Reading %s, %s (ID %d)" % (allSensors[sensorIndex][3], allSensors[sensorIndex][2], sensorIndex)
+  global temperature, humidity
   humidity, temperature = Adafruit_DHT.read_retry(allSensors[sensorIndex][0], allSensors[sensorIndex][1])
+  #Ensure you get a reading from the sensor before continuing.
   if humidity is not None and temperature is not None:
-
-    #output to influxdb
-    url = 'http://%s:%s/write?db=%s&precision=s' % (dbIP, dbPort, dbName)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    #output in Fahrenheit.
-    # if temperatureUnits is "F":
-    #   fahrenheitTemp = (temperature* 9)/5+32
-    #   temperaturePayload = "temperature,chamber=%s,sensor=%s,location=%s value=%.1f %d\n" % (chamberName, allSensors[sensorIndex][3], allSensors[sensorIndex][2], fahrenheitTemp, seconds)
-    #   print('Temp={0:0.1f}*F  Humidity={1:0.1f}%\n'.format(fahrenheitTemp, humidity))
-    # #output in Celcius.
-    # else:
-    temperaturePayload = "temperature,chamber=%s,sensor=%s,location=%s value=%.1f %d\n" % (chamberName, allSensors[sensorIndex][3], allSensors[sensorIndex][2], temperature, seconds)
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%\n'.format(temperature, humidity))
-    r = requests.post(url, data=temperaturePayload, headers=headers)
-    print temperaturePayload
-    humidityPayload = "humidity,chamber=%s,sensor=%s,location=%s value=%.1f %d\n" % (chamberName, allSensors[sensorIndex][3], allSensors[sensorIndex][2], humidity, seconds)
-    r = requests.post(url, data=humidityPayload, headers=headers)
-    print humidityPayload
+    #Do any relays need to be adjusted?
+    if sensorIndex is 0:
+      relayAdjustments()
+    #Output to Influxdb.
+    influxdbOutput()
   else:
     print('Failed to get reading. Try again!')
 
 
+#Function to output to Influxdb.
+def influxdbOutput():
+  url = 'http://%s:%s/write?db=%s&precision=s' % (dbIP, dbPort, dbName)
+  headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  temperaturePayload = "temperature,chamber=%s,sensor=%s,location=%s value=%.1f %d\n" % (chamberName, allSensors[sensorIndex][3], allSensors[sensorIndex][2], temperature, seconds)
+  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%\n'.format(temperature, humidity))
+  r = requests.post(url, data=temperaturePayload, headers=headers)
+  #print temperaturePayload
+  humidityPayload = "humidity,chamber=%s,sensor=%s,location=%s value=%.1f %d\n" % (chamberName, allSensors[sensorIndex][3], allSensors[sensorIndex][2], humidity, seconds)
+  r = requests.post(url, data=humidityPayload, headers=headers)
+  #print humidityPayload
+
+#Function to set relays based on sensor readings.
+def relayAdjustments():
+  print "Fridge relay = %s" % fridgeRelayState
+  print "Current temp = %.1f \nDesired temp = %.1f" %(temperature, desiredTemperature)
+  print "Temp range", (desiredTemperature + driftTemperature), "-", (desiredTemperature - driftTemperature)
+  if temperature > (desiredTemperature + driftTemperature):
+    GPIO.output(fridgeRelayPin,1)
+    print "Temperature too high. Turning on refrigerator."
+  elif temperature < (desiredTemperature - driftTemperature):
+    GPIO.output(fridgeRelayPin,0)
+    print "Temperature too low. Turning off refrigerator."
+
 #Loop through all sensors and get a reading.
 for sensorIndex, val in enumerate(allSensors):
   sensorReading()
-
-
-#Cleanup GPIO entries.
-GPIO.cleanup()
