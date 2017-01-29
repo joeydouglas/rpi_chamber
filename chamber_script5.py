dff4e2163cd813bceeec43c923357b52360fa356
Joey Douglas <joey.douglas@gmail.com>
2017-01-29 23:14:01 +0000

    added relay changes for heater and humidifier.

--- chamber_script5.py
+++ chamber_script5.py
@@ -9,16 +9,16 @@ import requests
 #Variables
 #---------
 #Database IP
-dbIP = "192.168.1.220"
+dbIP = "192.168.1.30"
 #Database port
 dbPort = "8086"
 #Influxdb database name
-dbName = "test"
+dbName = "meatsack"
 #Name of the curing chamber. Mostly useful if you have more than one.
 chamberName = "meatsack"
 #Desired temp/humidity set here.
-desiredTemperature = 20.5
-desiredHumidity = 80
+desiredTemperature = 22.5
+desiredHumidity = 45
 #Set the drift or fluctuation allowed before outputs are changed to adjust the temp/humidity.
 driftTemperature = 2
 driftHumidity = 2
@@ -44,22 +44,22 @@ allSensors = [[]]
 
 #Installed sensors. This will vary depending on your setup. Only one internal
 #temp/humidity sensor is required. Uncomment/change the values for up to 4 sensors.
-#Sensor 1
+#Sensor 1 - Primary sensor - will determine which devices are turned on/off.
 #--------
 sensor1Internal = AM2302
 sensor1InternalPin = 27
 allSensors[0] = [sensor1Internal, sensor1InternalPin, "internal", "sensor1"]
-#Sensor 2
+#Sensor 2 - Optional additional sensor.
 #--------
 sensor2Internal = DHT22
 sensor2InternalPin = 22
 allSensors.insert(1, [sensor2Internal, sensor2InternalPin, "internal", "sensor2"])
-#Sensor 3
+#Sensor 3 - Optional additional sensor.
 #--------
 sensor1External = AM2302
 sensor1ExternalPin = 27
 allSensors.insert(2, [sensor1External, sensor1ExternalPin, "external", "sensor1"])
-#Sensor 4
+#Sensor 4 - Optional additional sensor.
 #--------
 sensor2External = AM2302
 sensor2ExternalPin = 27
@@ -76,6 +76,11 @@ humidifierRelayPin = 0
 dehumidifierRelayPin = 0
 #Heater
 heaterRelayPin = 0
+
+##########################################################################
+#DO NOT EDIT BELOW This
+##########################################################################
+
 
 #Create Influxdb database. (This will probably be done when the Influxdb container is initialized, but for now....)
 url = 'http://%s:%s/query' % (dbIP, dbPort)
@@ -104,6 +109,18 @@ class Relay:
 fridge = Relay(fridgeRelayPin)
 fridge.setState(GPIO.input(fridge.pin))
 
+#Create heater object from Relayclass and get it's initial state.
+heater = Relay(heaterRelayPin)
+heater.setState(GPIO.input(heater.pin))
+
+#Create humidifier object from Relayclass and get it's initial state.
+humidifier = Relay(humidifierRelayPin)
+humidifier.setState(GPIO.input(humidifier.pin))
+
+#Create dehumidifier object from Relayclass and get it's initial state.
+dehumidifier = Relay(dehumidifierRelayPin)
+dehumidifier.setState(GPIO.input(dehumidifier.pin))
+
 #Function for getting temp/humidity readings.
 def sensorReading():
   print "Reading %s, %s (ID %d)" % (allSensors[sensorIndex][3], allSensors[sensorIndex][2], sensorIndex)
@@ -111,10 +128,10 @@ def sensorReading():
   humidity, temperature = Adafruit_DHT.read_retry(allSensors[sensorIndex][0], allSensors[sensorIndex][1])
   #Ensure you get a reading from the sensor before continuing.
   if humidity is not None and temperature is not None:
-    #Do any relays need to be adjusted?
+    #Adjust relay states based on primary sensor(0) reading.
     if sensorIndex is 0:
       relayAdjustments()
-    #Output to Influxdb.
+    #Output temp/humidity readings to Influxdb.
     influxSensorOutput()
   else:
     print('Failed to get reading. Try again!')
@@ -136,12 +153,19 @@ def influxSensorOutput():
 def influxRelayOutput():
   url = 'http://%s:%s/write?db=%s&precision=s' % (dbIP, dbPort, dbName)
   headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+  #fridge #####################
   fridgeRelayStatePayload = "fridgeRelayState,chamber=%s,stateChange=%d value=%d %d\n" % (chamberName, fridge.stateChange, fridge.relayState, seconds)
   r = requests.post(url, data=fridgeRelayStatePayload, headers=headers)
-  #print(fridgeRelayStatePayload)
-  #Heater
-  #Humidifier
-  #Dehumidifier
+  #heater #####################
+  heaterRelayStatePayload = "heaterRelayState,chamber=%s,stateChange=%d value=%d %d\n" % (chamberName, heater.stateChange, heater.relayState, seconds)
+  r = requests.post(url, data=heaterRelayStatePayload, headers=headers)
+  #humidifier #################
+  humidifierRelayStatePayload = "humidifierRelayState,chamber=%s,stateChange=%d value=%d %d\n" % (chamberName, humidifier.stateChange, humidifier.relayState, seconds)
+  r = requests.post(url, data=humidifierRelayStatePayload, headers=headers)
+  print(humidifierRelayStatePayload)
+  #dehumidifier
+  dehumidifierRelayStatePayload = "dehumidifierRelayState,chamber=%s,stateChange=%d value=%d %d\n" % (chamberName, dehumidifier.stateChange, dehumidifier.relayState, seconds)
+  r = requests.post(url, data=dehumidifierRelayStatePayload, headers=headers)
 
 
 #Function to set relays based on sensor readings.
@@ -150,21 +174,67 @@ def relayAdjustments():
   print "Fridge relay = %d" % fridge.relayState
   print "Current temp = %.1f \nDesired temp = %.1f" %(temperature, desiredTemperature)
   print "Temp range", (desiredTemperature + driftTemperature), "-", (desiredTemperature - driftTemperature)
+  #Turn fridge on if temperature is too high.
   if temperature > (desiredTemperature + driftTemperature):
     fridge.updateState(1)
     GPIO.output(fridge.pin,fridge.relayState)
+    heater.updateState(0)
+    GPIO.output(heater.pin,heater.relayState)
     print(fridge.relayState)
     print "Temperature too high. Turning on refrigerator."
     print "Fridge state change is %d" % fridge.stateChange
+  #Turn fridge off if temperature is too low.
   elif temperature < (desiredTemperature - driftTemperature):
     fridge.updateState(0)
     GPIO.output(fridge.pin,fridge.relayState)
+    heater.updateState(1)
+    GPIO.output(heater.pin,heater.relayState)
     print(fridge.relayState)
     print "Temperature too low. Turning off refrigerator."
     print "Fridge state change is %d" % fridge.stateChange
-  else:
-    #turn off heater and fridge.
+  #Turn on heater if temperature is way too low.
+  elif (desiredTemperature - driftTemperature) <= temperature <= (desiredTemperature + driftTemperature):
+    fridge.updateState(0)
+    GPIO.output(fridge.pin,fridge.relayState)
+    heater.updateState(0)
+    GPIO.output(heater.pin,heater.relayState)
+    print(heater.relayState)
     print "Temperature is within range!"
+  else:
+    #Turn off heater and fridge.
+    print "Temperature is within range!"
+
+  #Humidity
+  if humidity > (desiredHumidity + driftHumidity):
+    humidifier.updateState(0)
+    GPIO.output(humidifier.pin,humidifier.relayState)
+    dehumidifier.updateState(1)
+    GPIO.output(dehumidifier.pin,dehumidifier.relayState)
+    print(humidifier.relayState)
+    print "Humidity too high. Turning off humidifier."
+    print "humidifier state change is %d" % humidifier.stateChange
+  #Turn fridge off if temperature is too low.
+  elif humidity < (desiredHumidity - driftHumidity):
+    humidifier.updateState(1)
+    GPIO.output(humidifier.pin,humidifier.relayState)
+    dehumidifier.updateState(0)
+    GPIO.output(dehumidifier.pin,dehumidifier.relayState)
+    print(humidifier.relayState)
+    print "Humidity too low. Turning on humidifier."
+    print "humidifier state change is %d" % humidifier.stateChange
+  #Turn on heater if temperature is way too low.
+  elif (desiredHumidity - driftHumidity) <= humidity <= (desiredHumidity + driftHumidity):
+    humidifier.updateState(0)
+    GPIO.output(humidifier.pin,humidifier.relayState)
+    dehumidifier.updateState(0)
+    GPIO.output(dehumidifier.pin,dehumidifier.relayState)
+    print(humidifier.relayState)
+    print "Humidity is within range!"
+  else:
+    #Turn off heater and fridge.
+    print "Humidity is within range!"
+
+  #Send state information to Influxdb
   influxRelayOutput()
 
 #Loop through all sensors and get a reading.
