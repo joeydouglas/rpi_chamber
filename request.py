c275b1256c075c4292c44ae3f7e32164d7d1499c
joeyd <joey.douglas@gmail.com>
2017-03-04 00:01:09 +0000

    more cleanup

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = 'http://192.168.1.220:8086/write?db=meatsack'
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-payload = "tested02,host=server01,region=us-west value=0.64 1434055562000000000\n"
-r = requests.post(url, data=payload, headers=headers)
-r.raw
